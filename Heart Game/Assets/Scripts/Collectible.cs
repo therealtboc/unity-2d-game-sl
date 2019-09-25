5d9565199a0943a2c99f3490767ad319522fa787
therealtboc <samuelleach78@webster.edu>
2019-09-25 15:27:27 -0500

    Initial project commit

---
+++ Heart Game/Assets/Scripts/Collectible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HandleCollected();
+    }
+    void HandleCollected()
+    {
+        //print("Handle the pickup");
+        CollectibleManager.Instance.HandleHeartCollected();
+        Destroy(gameObject);
+    }
+}
---
+++ Heart Game/Assets/Scripts/CollectibleManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectibleManager : MonoBehaviour
+{
+    public static CollectibleManager Instance;
+
+    private int _totalHearts;
+    private int _collectedHearts;
+
+    public TextMeshProUGUI heartCount;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        _totalHearts = FindObjectsOfType<Collectible>().Length;     //Not viable for larger implementations, think about calling from arrays
+        DisplayHeartCount();
+    }
+
+    void DisplayHeartCount()
+    {
+        heartCount.SetText(_collectedHearts + " / " + _totalHearts);
+        if(_collectedHearts == _totalHearts)
+        {
+            heartCount.SetText("All Hearts Collected!");
+        }
+    }
+
+    public void HandleHeartCollected()
+    {
+        _collectedHearts++;
+        DisplayHeartCount();
+    }
+}
